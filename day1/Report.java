e4d06224287f1342b4368e01736cd9c5d7ae68f4
Devapriyasharma <43360907+Devapriyasharma@users.noreply.github.com>
2023-06-17 22:15:37 -0400

    Add files via upload

---
+++ day1/Report.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student result = new Student();
+		System.out.println(result.studentName);
+		System.out.println(result.rollNo);
+		System.out.println(result.collegeName);
+		System.out.println(result.markScored);
+		System.out.println(result.cgpa);
+	
+	}
+
+}
--- day1/Student.java
+++ day1/Student.java
@@ -1,10 +1,24 @@
 package week1.day1;
 
 public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
+	String studentName = "Devapriya";
+	int rollNo = 43;
+	String collegeName = "SRMIST"; 
+	char markScored = 'A';
+	float cgpa = 8.8f;
+	
+	
+	
 
 }
+
+//Create Class Student
+//declare variables
+//studentName
+//rollNo
+//collegeName
+//markScored
+//cgpa
+//
+//Create a class Report and create object for student class in main method and print all the variables
